d8c1ec55276a9872feffb53470f0be336dedb64d
Jerrycull00 <jerrycull00@gmail.com>
2023-06-07 16:12:06 -0700

    puts recursion

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _puts_recursion - put recursive functions
+ * @s: argument
+ * Return: 0 always
+ *
+ */
+
+void _puts_recursion(char *s)
+{
+	if (*s)
+	{
+		_putchar(*s);
+		_puts_recursion(s + 1);
+	}
+	if (!*s)	
+		_putchar('\n');
+}
---
+++ 0x08-recursion/README.md
@@ -0,0 +1,1 @@
+Rcursive Function project
