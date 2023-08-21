33290f130bbd43e4a66d3fd3c388ca8b93c16554
Jerrycull00 <onaolapojeremiah14@gmail.com>
2023-08-21 15:01:05 -0700

    0x18-dynamic_libraries

---
+++ 0x18-dynamic_libraries/4-strpbrk.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * _strpbrk - this is the entry point
+ * @s: input
+ * @accept: input
+ * Return: always 0 (success)
+ */
+
+char *_strpbrk(char *s, char *accept)
+{
+		int a;
+
+		while (*s)
+		{
+			for (a = 0; accept[a]; a++)
+			{
+			if (*s == accept[a])
+			return (s);
+			}
+		s++;
+		}
+
+	return ('\0');
+}
